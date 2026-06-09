072e589f8138336a6a34b542ed2b9de208a3e248
Khoideptrai1 <khoideptrai1410@gmail.com>
2026-06-09 13:37:16 +0700

    Update INotificationHub.cs

--- Backend/Rhythmix.Application/Common/Interface/INotificationHub.cs
+++ Backend/Rhythmix.Application/Common/Interface/INotificationHub.cs
@@ -1,1 +1,9 @@
+using System.Threading.Tasks;
 
+namespace TuneVault.Application.Common.Interfaces
+{
+    public interface INotificationHub
+    {
+        Task SendNotification(string userId, object notification);
+    }
+}
