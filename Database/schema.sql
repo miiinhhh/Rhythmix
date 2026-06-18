5e67bbc30e9157f22320524b6aec620bb2cf40ae
miiinhhh <minhming5542@gmail.com>
2026-06-18 09:50:59 +0700

    add Artist và seed dữ liệu vào db vào db

--- Database/schema.sql
+++ Database/schema.sql
@@ -19,14 +19,14 @@ CREATE TABLE UserProfiles
         REFERENCES AspNetUsers(Id)
 );
 
--- CREATE TABLE Artists
--- (
---     ArtistId UNIQUEIDENTIFIER PRIMARY KEY,
---     Name NVARCHAR(200) NOT NULL,
---     Description NVARCHAR(MAX),
---     AvatarUrl NVARCHAR(500),
---     CreatedAt DATETIME2 DEFAULT GETDATE()
--- )
+CREATE TABLE Artists
+(
+    ArtistId UNIQUEIDENTIFIER PRIMARY KEY,
+    Name NVARCHAR(200) NOT NULL,
+    Description NVARCHAR(MAX),
+    AvatarUrl NVARCHAR(500),
+    CreatedAt DATETIME2 DEFAULT GETDATE()
+);
 
 CREATE TABLE Albums
 (
@@ -60,6 +60,7 @@ CREATE TABLE MediaItems
     ThumbnailUrl NVARCHAR(500),
     MimeType NVARCHAR(100),
     FileSize BIGINT,
+    ArtistId UNIQUEIDENTIFIER NULL,
     AlbumId UNIQUEIDENTIFIER NULL,
     GenreId UNIQUEIDENTIFIER NULL,
     OwnerId UNIQUEIDENTIFIER NOT NULL,
@@ -67,6 +68,9 @@ CREATE TABLE MediaItems
     ViewCount INT DEFAULT 0,
     CreatedAt DATETIME2 DEFAULT GETDATE(),
 
+    FOREIGN KEY (ArtistId)
+        REFERENCES Artists(ArtistId),
+        
     FOREIGN KEY (AlbumId)
         REFERENCES Albums(AlbumId),
 
@@ -185,20 +189,3 @@ CREATE TABLE Follows
         REFERENCES AspNetUsers(Id)
 );
 
-CREATE TABLE PlayListTrack
-(
-    PlaylistId UNIQUEIDENTIFIER NOT NULL,
-    MediaId UNIQUEIDENTIFIER NOT NULL,
-    SortOrder INT DEFAULT 0,    -- Cột thêm vào: Giúp kéo thả, sắp xếp thứ tự bài hát
-    
-    PRIMARY KEY (PlaylistId, MediaId), 
-
-    CONSTRAINT FK_PlaylistItems_Playlists
-        FOREIGN KEY (PlaylistId) REFERENCES Playlists(PlaylistId) 
-        ON DELETE CASCADE, -- Nếu xóa playlist thì tự động xóa các liên kết bên trong
-
-    -- Khóa ngoại liên kết tới bảng MediaItems (Track)
-    CONSTRAINT FK_PlaylistItems_MediaItems
-        FOREIGN KEY (MediaId) REFERENCES MediaItems(MediaId)
-        ON DELETE CASCADE -- Nếu bài hát bị xóa khỏi hệ thống thì tự động biến mất khỏi playlist
-);
