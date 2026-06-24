d086ef564c576cc6ab16960185fc68192904d936
miiinhhh <minhming5542@gmail.com>
2026-06-24 21:13:20 +0700

    upd tên dự án trên side bar bên trái, sửa lightmode thành sáng mờ

--- Frontend/src/pages/SearchPage.tsx
+++ Frontend/src/pages/SearchPage.tsx
@@ -95,7 +95,7 @@ const SearchPage = () => {
   const [followedArtistIds, setFollowedArtistIds] = useState<Record<string, boolean>>({});
   const artistCoverInputRef = useRef<HTMLInputElement>(null);
   const [isSearching, setIsSearching] = useState(false);
-  const { setCurrentSongId, setIsPlaying, songs, setSongs } = useOutletContext<OutletContextType>();
+  const { setCurrentSongId, setIsPlaying, setSongs } = useOutletContext<OutletContextType>();
 
   useEffect(() => {
     userService.getUsers().then(setUsers).catch(() => setUsers([]));
